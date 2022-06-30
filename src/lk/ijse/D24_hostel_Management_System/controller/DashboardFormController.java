877215b4a48494fa4a6c8598aafdb39baed34116
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-06-30 12:09:15 +0530

    Created Basic UI form for DashboardForm

--- src/lk/ijse/D24_hostel_Management_System/controller/DashboardFormController.java
+++ src/lk/ijse/D24_hostel_Management_System/controller/DashboardFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.D24_hostel_Management_System.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardFormController {
+    public AnchorPane DashboardForm;
+
+    public void manageStudentbtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void manageRoomsbtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void checkAvailableRoomsOnaction(ActionEvent actionEvent) {
+    }
+
+    public void RoomReservationbtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void LogOutbtnOnAction(ActionEvent actionEvent) {
+    }
 }
