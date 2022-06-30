55fdb3d8e26629c59976d6606ac863114fd0ebce
[200~Rashmiya~ <rashmiyarafeek1200@gmail.com>
2022-06-30 18:18:36 +0530

    Refactored CrudDAO interface

--- src/lk/ijse/D24_hostel_Management_System/controller/StudentFormController.java
+++ src/lk/ijse/D24_hostel_Management_System/controller/StudentFormController.java
@@ -6,13 +6,19 @@ import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import lk.ijse.D24_hostel_Management_System.entity.Student;
 import lk.ijse.D24_hostel_Management_System.util.Loader;
+import lk.ijse.D24_hostel_Management_System.view.tm.StudentTM;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class StudentFormController implements Loader {
     public AnchorPane manageStudentForm;
@@ -29,7 +35,56 @@ public class StudentFormController implements Loader {
     public TableColumn colDOB;
     public TableColumn colGender;
     public TableColumn colModify;
-    public TableView<Student> tblStudent;
+    public TableView<StudentTM> tblStudent;
+    public JFXDatePicker datePickerDOB;
+    public JFXComboBox cmbGender;
+    public Button btnSave;
+    public Button btnUpdate;
+
+    public void initialize(){
+       /* colID.setCellValueFactory(new PropertyValueFactory("id"));
+        colName.setCellValueFactory(new PropertyValueFactory("name"));
+        colAddress.setCellValueFactory(new PropertyValueFactory("address"));
+        colSalary.setCellValueFactory(new PropertyValueFactory("salary"));
+
+
+        try {
+            loadAllStudents();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }*/
+    }
+
+    private void loadAllStudents() {
+        /*tblCustomers.getItems().clear();
+        *//*Get all customers*//*
+        try {
+            // loose Coupling
+            ArrayList<CustomerDTO> allCustomers = customerBO.getAllCustomers();
+
+            for (CustomerDTO customer: allCustomers
+            ) {
+                tblCustomers.getItems().add(new CustomerTM(customer.getId(),customer.getName(),customer.getAddress()));
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        } catch (ClassNotFoundException e) {
+            new Alert(Alert.AlertType.ERROR, e.getMessage()).show();
+        }*/
+    }
+
+    private void clearFields() {
+        txtId.clear();
+        txtName.clear();
+        txtAddress.clear();
+        txtContact.clear();
+        datePickerDOB.setValue(null);
+        cmbGender.setValue(null);
+        btnUpdate.setDisable(true);
+        btnSave.setDisable(false);
+    }
 
     public void backToDashboardbtnOnAction(ActionEvent actionEvent) throws IOException {
             loadUi("DashboardForm");
@@ -40,5 +95,12 @@ public class StudentFormController implements Loader {
         manageStudentForm.getChildren().clear();
         Parent parent = FXMLLoader.load(getClass().getResource("../view/" + location + ".fxml"));
         manageStudentForm.getChildren().add(parent);
+    }
+
+    public void saveStudentOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void updateStudentOnAction(ActionEvent actionEvent) {
     }
 }
